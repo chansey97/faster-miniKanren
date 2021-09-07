f4037c62d84a62ee8cd7e8fee74f7dec90197c89
Siyuan Chen <chansey97@126.com>
2021-09-07 22:31:10 +0800

    Move require.scm to tests and add an empty basic-tests.scm.

--- require.scm
+++
@@ -1,5 +0,0 @@
-(load "mk-vicare.scm")
-(load "mk.scm")
-(load "smt.scm")
-(load "z3-server.scm")
-(load "test-check.scm")
---
+++ tests/require.scm
@@ -0,0 +1,5 @@
+(load "../mk-vicare.scm")
+(load "../mk.scm")
+(load "../smt.scm")
+(load "../z3-server.scm")
+(load "../test-check.scm")
