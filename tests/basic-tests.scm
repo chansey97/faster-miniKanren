09ec63edcea93b2a96ef7f570d0973cd8b69282e
Siyuan Chen <chansey97@126.com>
2021-09-08 22:56:52 +0800

    Remove comments about SMT logs.

--- tests/basic-tests.scm
+++ tests/basic-tests.scm
@@ -4,36 +4,11 @@
             (z/ `(declare-const ,a Int)) ;
             (z/assert `(= ,a 5))
             ))
-;; (reset)
-;; (declare-const _v2 Int)
-;; (declare-const _a1 Bool)
-;; (assert (=> _a1 (= _v2 5)))
-;; (check-sat-assuming (_a1))
-;; sat
-;; (check-sat-assuming (_a1))
-;; sat
-;; (get-model)
-;; (
-;;   model
-;;   (define-fun _a1 () Bool true)
-;;   (define-fun _v2 () Int 5)
-;; )
-;; (declare-const _a2 Bool)
-;; (assert (=> _a2 (= _v2 5)))
-;; (check-sat-assuming ((not _a1) _a2))
-;; sat
 
 (run 1 (q)
      (fresh (a b)
             (z/assert `(= ,a 5))
             ))
-;; (reset)
-;; (declare-const _a1 Bool)
-;; (assert (=> _a1 (= _v1 5)))
-;; (check-sat-assuming (_a1))
-;; (error line 3 column 19: unknown constant _v1)
-;;
-;; Correct!
 
 (run 1 (q)
      (fresh (a b)
@@ -42,24 +17,6 @@
             (z/assert `(= ,a 5))
             (z/ `(declare-const ,b Int))
             ))
-;; (reset)
-;; (declare-const _v2 Int)
-;; (declare-const _a1 Bool)
-;; (assert (=> _a1 (= _v2 5)))
-;; (check-sat-assuming (_a1))
-;; sat
-;; (check-sat-assuming (_a1))
-;; sat
-;; (get-model)
-;; (
-;;   model
-;;   (define-fun _a1 () Bool true)
-;;   (define-fun _v2 () Int 5)
-;; )
-;; (declare-const _a2 Bool)
-;; (assert (=> _a2 (= _v2 5)))
-;; (check-sat-assuming ((not _a1) _a2))
-;; sat
 
 (run 3 (q)
      (conde
@@ -69,50 +26,6 @@
        (z/assert `(= ,q 2)))
       ))
 ;; (1 2)
-;; (reset)
-;; (declare-const _v0 Int)
-;; (declare-const _a1 Bool)
-;; (assert (=> _a1 (= _v0 1)))
-;; (check-sat-assuming (_a1))
-;; sat
-;; (check-sat-assuming (_a1))
-;; sat
-;; (get-model)
-;; (
-;;   model
-;;   (define-fun _v0 () Int 1)
-;;   (define-fun _a1 () Bool true)
-;; )
-;; (declare-const _a2 Bool)
-;; (assert (=> _a2 (= _v0 1)))
-;; (check-sat-assuming ((not _a1) _a2))
-;; sat
-;; (declare-const _a3 Bool)
-;; (assert (=> _a3 (= _v0 2)))
-;; (check-sat-assuming ((not _a1) (not _a2) _a3))
-;; sat
-;; (check-sat-assuming ((not _a1) (not _a2) _a3))
-;; sat
-;; (get-model)
-;; (
-;;   model
-;;   (define-fun _a3 () Bool true)
-;;   (define-fun _v0 () Int 2)
-;;   (define-fun _a1 () Bool false)
-;;   (define-fun _a2 () Bool false)
-;; )
-;; (declare-const _a4 Bool)
-;; (assert (=> _a4 (= _v0 2)))
-;; (check-sat-assuming ((not _a1) (not _a2) (not _a3) _a4))
-;; sat
-;; (declare-const _a5 Bool)
-;; (assert (=> _a5 (or (not (= _v0 1)))))
-;; (check-sat-assuming (_a1 (not _a2) (not _a3) (not _a4) _a5))
-;; unsat
-;; (declare-const _a6 Bool)
-;; (assert (=> _a6 (or (not (= _v0 2)))))
-;; (check-sat-assuming ((not _a1) (not _a2) _a3 (not _a4) (not _a5) _a6))
-;; unsat
 
 (run 1 (q)
      (fresh (a b)
@@ -171,28 +84,6 @@
             (add1o n 1)
             (== q n)))
 ;; (0)
-;; (reset)
-;; (declare-const _v1 Int)
-;; (declare-const _a1 Bool)
-;; (assert (=> _a1 (= 1 (+ _v1 1))))
-;; (check-sat-assuming (_a1))
-;; sat
-;; (check-sat-assuming (_a1))
-;; sat
-;; (get-model)
-;; (
-;;   model
-;;   (define-fun _v1 () Int 0)
-;;   (define-fun _a1 () Bool true)
-;; )
-;; (declare-const _a2 Bool)
-;; (assert (=> _a2 (= _v1 0)))
-;; (check-sat-assuming ((not _a1) _a2))
-;; sat
-;; (declare-const _a3 Bool)
-;; (assert (=> _a3 (or (not (= _v1 0)))))
-;; (check-sat-assuming (_a1 (not _a2) _a3))
-;; unsat
 
 (define (nevero)
   (conde
@@ -212,21 +103,6 @@
             
             (nevero)))
 ;; ()
-;; (reset)
-;; (declare-const _v1 Int)
-;; (declare-const _a1 Bool)
-;; (assert (=> _a1 (= _v1 5)))
-;; (check-sat-assuming (_a1))
-;; sat
-;; (declare-const _v2 Int)
-;; (declare-const _a2 Bool)
-;; (assert (=> _a2 (= _v2 6)))
-;; (check-sat-assuming (_a1 _a2))
-;; sat
-;; (declare-const _a3 Bool)
-;; (assert (=> _a3 (= _v1 _v2)))
-;; (check-sat-assuming (_a1 _a2 _a3))
-;; unsat
 
 (define (nevero)
   (conde
@@ -246,21 +122,6 @@
 
             (nevero)))
 ;; ()
-;; (reset)
-;; (declare-const _v1 Int)
-;; (declare-const _a1 Bool)
-;; (assert (=> _a1 (= _v1 5)))
-;; (check-sat-assuming (_a1))
-;; sat
-;; (declare-const _v2 Int)
-;; (declare-const _a2 Bool)
-;; (assert (=> _a2 (= _v2 5)))
-;; (check-sat-assuming (_a1 _a2))
-;; sat
-;; (declare-const _a3 Bool)
-;; (assert (=> _a3 (and (or (not (= _v1 _v2))))))
-;; (check-sat-assuming (_a1 _a2 _a3))
-;; unsat
 
 (run 3 (q)
      (fresh (a b)
@@ -269,16 +130,6 @@
             (z/assert `(= ,a 5)) ; <-- won't actually promote the above `=/=` to SMT assert, because b is not a SMT variable
             (== q `(,a ,b))))
 ;; (((5 _.0) (=/= ((_.0 5)))))
-;; (reset)
-;; (declare-const _v1 Int)
-;; (declare-const _a1 Bool)
-;; (assert (=> _a1 (= _v1 5)))
-;; (check-sat-assuming (_a1))
-;; sat
-;; (declare-const _a2 Bool)
-;; (assert (=> _a2 (= _v1 6)))
-;; (check-sat-assuming (_a1 _a2))
-;; unsat
 
 (define (nevero)
   (conde
@@ -298,21 +149,6 @@
             
             (nevero)))
 ;; ()
-;; (reset)
-;; (declare-const _v1 Int)
-;; (declare-const _a1 Bool)
-;; (assert (=> _a1 (= _v1 5)))
-;; (check-sat-assuming (_a1))
-;; sat
-;; (declare-const _v2 Int)
-;; (declare-const _a2 Bool)
-;; (assert (=> _a2 (= _v2 5)))
-;; (check-sat-assuming (_a1 _a2))
-;; sat
-;; (declare-const _a3 Bool)
-;; (assert (=> _a3 (and (or (not (= _v1 _v2))))))
-;; (check-sat-assuming (_a1 _a2 _a3))
-;; unsat
 
 (run 1 (q)
      (fresh (a b)
@@ -322,17 +158,6 @@
             (z/assert `(= (- ,a ,b) 0))  ; <-- promote the above `=/=` to SMT assert 
             ))
 ;; ()
-;; (reset)
-;; (declare-const _v1 Int)
-;; (declare-const _v2 Int)
-;; (declare-const _a1 Bool)
-;; (assert (=> _a1 (= (- _v1 _v2) 0)))
-;; (check-sat-assuming (_a1))
-;; sat
-;; (declare-const _a2 Bool)
-;; (assert (=> _a2 (and (or (not (= _v1 _v2))))))
-;; (check-sat-assuming (_a1 _a2))
-;; unsat
 
 (define (nevero)
   (conde
@@ -354,25 +179,6 @@
             (nevero)
             ))
 ;; ()
-;; (reset)
-;; (declare-const _v1 Int)
-;; (declare-const _v3 Int)
-;; (declare-const _a1 Bool)
-;; (assert (=> _a1 (< _v1 2)))
-;; (check-sat-assuming (_a1))
-;; sat
-;; (declare-const _a2 Bool)
-;; (assert (=> _a2 (> _v1 0)))
-;; (check-sat-assuming (_a1 _a2))
-;; sat
-;; (declare-const _a3 Bool)
-;; (assert (=> _a3 (= _v3 1)))
-;; (check-sat-assuming (_a1 _a2 _a3))
-;; sat
-;; (declare-const _a4 Bool)
-;; (assert (=> _a4 (and (or (not (= _v1 _v3))))))
-;; (check-sat-assuming (_a1 _a2 _a3 _a4))
-;; unsat
 
 (run 3 (q)
      (fresh (a b c d e f)
@@ -450,14 +256,3 @@
             ; because if `b` is fresh, the type of `b` must be equal to the type of `a`.
             ; so ideally miniKanren can automatically declare `b`.
             ))
-;; (reset)
-;; (declare-const _v1 Int)
-;; (declare-const _a1 Bool)
-;; (assert (=> _a1 (= _v1 5)))
-;; (check-sat-assuming (_a1))
-;; sat
-;; (declare-const _a2 Bool)
-;; (assert (=> _a2 (= _v1 _v2)))
-;; (check-sat-assuming (_a1 _a2))
-;; (error line 7 column 23: unknown constant _v2)
-
