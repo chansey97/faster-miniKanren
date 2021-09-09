16a015866de9d49cbe7273ecdbcb0e43c30b3cb2
Siyuan Chen <chansey97@gmail.com>
2021-09-09 18:29:36 +0800

    Rewrite it by more integrated SMT interfaces (all passed).
    `faco` and `facto` cp from https://github.com/namin/clpsmt-miniKanren/blob/master/talk.scm e9a2ae0005d32da0fdb052748fc5fcc00227110d

--- smt-extra-tests.scm
+++ smt-extra-tests.scm
@@ -1,8 +1,15 @@
+(load "mk-vicare.scm")
+(load "mk.scm")
+(load "smt.scm")
+(load "z3-server.scm")
+(load "test-check.scm")
+
 (test "conde"
       (run 3 (q)
+           (smt-typeo q 'Int)
            (conde
-            ((z/assert `(= ,q 1)))
-            ((z/assert `(= ,q 2)))))
+            ((smt-asserto `(= ,q 1)))
+            ((smt-asserto `(= ,q 2)))))
       '(1 2))
 
 (test "join"
@@ -12,14 +19,17 @@
                   (conde
                    ((== a 4))
                    ((== a 2)))
-                  (z/assert `(= ,a (+ ,b ,b)))))
+                  (smt-typeo a 'Int)
+                  (smt-typeo b 'Int)
+                  (smt-asserto `(= ,a (+ ,b ,b)))))
       '((4 2) (2 1)))
 
 (test "unif"
       (run 2 (q)
            (fresh (a b)
                   (== q `(,a ,b))
-                  (z/assert `(> ,a 0))
+                  (smt-typeo a 'Int)
+                  (smt-asserto `(> ,a 0))
                   (== b 2)
                   (== b a)))
       '((2 2)))
@@ -28,10 +38,41 @@
       (run 2 (q)
            (fresh (a b)
                   (== q `(,a ,b))
-                  (z/assert `(> ,a 0))
+                  (smt-typeo a 'Int)
+                  (smt-asserto `(> ,a 0))
                   (== b a)
                   (== b 2)))
       '((2 2)))
+
+(define faco
+  (lambda (n out)
+    (fresh ()
+           (smt-typeo n 'Int)
+           (smt-typeo out 'Int)
+           (conde ((smt-asserto `(= ,n 0))
+                   (smt-asserto `(= ,out 1)))
+                  ((smt-asserto `(> ,n 0))
+                   (fresh (n-1 r)
+                          (smt-typeo n-1 'Int)
+                          (smt-typeo r 'Int)
+                          (smt-asserto `(= (- ,n 1) ,n-1))
+                          (smt-asserto `(= (* ,n ,r) ,out))
+                          (faco n-1 r)))))))
+
+;; equivalent
+(define facto
+  (lambda (n out)
+    (conde ((== n 0)
+            (== out 1))
+           ((smt-typeo n 'Int)
+            (smt-asserto `(> ,n 0))
+            (fresh (n-1 r)
+                   (smt-typeo n-1 'Int)
+                   (smt-typeo r 'Int)
+                   (smt-typeo out 'Int)
+                   (smt-asserto `(= (- ,n 1) ,n-1))
+                   (smt-asserto `(= (* ,n ,r) ,out))
+                   (facto n-1 r))))))
 
 (test "faco-0"
       (run* (q)
@@ -57,50 +98,56 @@
             (facto n out)
             (== q `(,n ,out))))
 
-(load "../clpsmt-miniKanren/full-interp-with-let.scm")
+(load "full-interp-with-let.scm")
 (test "evalo-backwards-fib-quoted-6"
-      (run 1 (q)
-           (evalo `(letrec ((f
-                             (lambda (n)
-                               (if (< n 0) #f
-                                   (if (< n 2) n
-                                       (+ (f (- n 1)) (f (- n 2))))))))
-                     (f ',q))
-                  8))
-      '(6))
+  (run 1 (q)
+    (evalo `(letrec ((f
+                      (lambda (n)
+                        (if (< n 0) #f
+                            (if (< n 2) n
+                                (+ (f (- n 1)) (f (- n 2))))))))
+              (f ',q))
+           8))
+  '(6))
 
 (test "fail-1"
       (run 1 (x)
            (=/= x 2)
-           (z/assert `(> ,x 1))
-           (z/assert `(< ,x 3)))
+           (smt-typeo x 'Int)
+           (smt-asserto `(> ,x 1))
+           (smt-asserto `(< ,x 3)))
       '())
 
 (test "fail-2"
       (run 1 (x)
            (absento 2 x)
-           (z/assert `(> ,x 1))
-           (z/assert `(< ,x 3)))
+           (smt-typeo x 'Int)
+           (smt-asserto `(> ,x 1))
+           (smt-asserto `(< ,x 3)))
       '())
 
 (test "fail-3"
       (run 2 (x)
            (=/= x 2)
-           (z/assert `(= 4 (* ,x ,x))))
+           (smt-typeo x 'Int)
+           (smt-asserto `(= 4 (* ,x ,x))))
       '(-2))
 
 (test "real-1"
       (run 1 (x)
-           (z/ `(declare-const ,x Real))
-           (z/assert `(= 4.2 (* 2 ,x))))
+           (smt-typeo x 'Real)
+           (smt-asserto `(= 4.2 (* 2 ,x))))
       '(2.1))
 
 (test "diseq-0"
       (run 1 (q)
            (fresh (x y)
-                  (z/assert `(> ,x 0))
-                  (z/assert `(> ,y 0))
-                  (z/assert `(= ,q (* ,x ,y)))))
+                  (smt-typeo x 'Int)
+                  (smt-typeo y 'Int)
+                  (smt-typeo q 'Int)
+                  (smt-asserto `(> ,x 0))
+                  (smt-asserto `(> ,y 0))
+                  (smt-asserto `(= ,q (* ,x ,y)))))
       '(1))
 
 (test "diseq-1"
@@ -108,18 +155,24 @@
            (fresh (x y)
                   (=/= x 0)
                   (=/= y 0)
-                  (z/assert `(>= ,x 0))
-                  (z/assert `(>= ,y 0))
-                  (z/assert `(= ,q (* ,x ,y)))))
+                  (smt-typeo x 'Int)
+                  (smt-typeo y 'Int)
+                  (smt-typeo q 'Int)
+                  (smt-asserto `(>= ,x 0))
+                  (smt-asserto `(>= ,y 0))
+                  (smt-asserto `(= ,q (* ,x ,y)))))
       '(1))
 
 
 (test "diseq-2"
       (run 1 (q)
            (fresh (x y)
-                  (z/assert `(>= ,x 0))
-                  (z/assert `(>= ,y 0))
-                  (z/assert `(= ,q (* ,x ,y)))
+                  (smt-typeo x 'Int)
+                  (smt-typeo y 'Int)
+                  (smt-typeo q 'Int)
+                  (smt-asserto `(>= ,x 0))
+                  (smt-asserto `(>= ,y 0))
+                  (smt-asserto `(= ,q (* ,x ,y)))
                   (=/= x 0)
                   (=/= y 0)))
       '(1))
@@ -129,13 +182,17 @@
            (fresh (x y)
                   (=/= x 0)
                   (=/= y 0)
-                  (z/assert `(= 0 (* ,x ,y)))))
+                  (smt-typeo x 'Int)
+                  (smt-typeo y 'Int)
+                  (smt-asserto `(= 0 (* ,x ,y)))))
       '())
 
 (test "diseq-4"
       (run 1 (q)
            (fresh (x y)
-                  (z/assert `(= 0 (* ,x ,y)))
+                  (smt-typeo x 'Int)
+                  (smt-typeo y 'Int)
+                  (smt-asserto `(= 0 (* ,x ,y)))
                   (=/= x 0)
                   (=/= y 0)))
       '())
