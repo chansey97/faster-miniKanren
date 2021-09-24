fe6d43062a57c746188558a42e0e720f1558f90f
Siyuan Chen <chansey97@outlook.com>
2021-09-25 00:56:38 +0800

    Add require.scm.

---
+++ require.scm
@@ -0,0 +1,23 @@
+(load "mk-vicare.scm")
+(load "mk.scm")
+(load "match.scm")
+(load "smt.scm")
+(load "z3-server.scm")
+(load "test-check.scm")
+
+;(mode 'naive)
+;(mode `(assumptions 1000 #f))  ; don't assert negative assms. seems faster.
+;(mode `(assumptions 1000 #t))  ; asssert negative assms
+;(mode 'push-pop)
+(mode 'reset-as-pop-push)
+
+(displayln (mode))
+
+
+;; (load "smt-extra-tests.scm")
+;; (load "talk.scm")
+;; (load "radi-tests.scm")
+;; (load "radiw-concrete-tests.scm")
+;; (load "full-abstract-interp-extended-tests.scm")
+;; (load "rsa.scm")
+;; (load "property-based-synthesis-tests.scm")
