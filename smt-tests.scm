df9fc52b4cc80de3c4db5901523032b5f3e88cee
Siyuan Chen <chansey97@outlook.com>
2021-09-24 18:50:33 +0800

    smt-tests.scm, now collects all the SMT tests.

--- smt-tests.scm
+++ smt-tests.scm
@@ -1,20 +1,84 @@
-(load "mk-vicare.scm")
-(load "mk.scm")
-(load "smt.scm")
-(load "../clpsmt-miniKanren/test-check.scm")
+(load "require.scm")
 
-;;(load "../clpsmt-miniKanren/z3-driver.scm")
-;;(load "../clpsmt-miniKanren/cvc4-driver.scm")
-(load "../clpsmt-miniKanren/z3-server-robust.scm")
-;;(load "../clpsmt-miniKanren/cvc4-server-robust.scm")
-(load "../clpsmt-miniKanren/talk.scm")
-(load "../clpsmt-miniKanren/radi-tests.scm")
-(load "../clpsmt-miniKanren/radiw-concrete-tests.scm")
-;;(load "../clpsmt-miniKanren/full-abstract-interp-extended-tests.scm")
-;;(load "../clpsmt-miniKanren/rsa.scm")
-;;(load "../clpsmt-miniKanren/property-based-synthesis-tests.scm")
-;;(load "../clpsmt-miniKanren/mk.scm")
-;;(load "../clpsmt-miniKanren/z3-driver.scm")
-;;slow
-;;(load "../clpsmt-miniKanren/twenty-four-puzzle.scm")
+(printf "smt-extra-tests.scm\n")
 (load "smt-extra-tests.scm")
+
+(printf "clpsmt-basic-tests.scm\n")
+(load "clpsmt-basic-tests.scm")
+
+(printf "clpsmt-tests.scm\n")
+(load "clpsmt-tests.scm")
+
+
+(printf "full-interp-extended-memo-lambda-tests.scm\n")
+(load "full-interp-extended-memo-lambda-tests.scm")
+
+(printf "full-interp-extended-tests.scm\n")
+(load "full-interp-extended-tests.scm")
+
+(printf "full-interp-extended-with-amb-tests.scm\n")
+(load "full-interp-extended-with-amb-tests.scm")
+
+(printf "full-interp-mutation-apply-tests.scm\n")
+(load "full-interp-mutation-apply-tests.scm")
+
+
+(printf "talk.scm\n")
+(load "talk.scm")
+
+(printf "program-synthesizer-blog-post.scm\n")
+(load "program-synthesizer-blog-post.scm")
+
+(printf "property-based-synthesis-tests.scm\n")
+(load "property-based-synthesis-tests.scm")
+
+(printf "symbolic-execution-tests.scm\n")
+(load "symbolic-execution-tests.scm")
+
+(printf "synthesis.scm\n")
+(load "synthesis.scm")
+
+(printf "while-abort-tests.scm\n")
+(load "while-abort-tests.scm")
+
+
+(printf "radi-tests.scm\n")
+(load "radi-tests.scm")
+
+(printf "radif-tests.scm\n")
+(load "radif-tests.scm")
+
+(printf "radiw-concrete-tests.scm\n")
+(load "radiw-concrete-tests.scm")
+
+(printf "radiw-tests.scm\n")
+(load "radiw-tests.scm")
+
+
+(printf "old-twenty-four-puzzle.scm\n") 
+(load "old-twenty-four-puzzle.scm")
+
+(printf "twenty-four-puzzle.scm\n")
+(load "twenty-four-puzzle.scm")
+
+(printf "twenty-four-puzzle-depth-limit.scm\n")
+(load "twenty-four-puzzle-depth-limit.scm")
+
+(printf "twenty-four-puzzle-smart.scm\n")
+(load "twenty-four-puzzle-smart.scm")
+
+
+(printf "kcoloring.scm\n")
+(load "kcoloring.scm")
+
+(printf "music.scm\n")
+(load "music.scm")
+
+(printf "rsa.scm\n")
+(load "rsa.scm")
+
+;; TODO: More tests (e.g. More types, reactive, non-z3 tests, tabling, etc):
+;; (load "sign-domain-tests.scm")
+;; (load "abstract-interp.scm")
+;; (load "abstract-interp-tagged.scm")
+;; ...
